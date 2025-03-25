d11919aa9876e3c8f18a1c9fcd9fd06c7e1312ed
Craig Barnes <craig.barnes@intelligentlight.com>
2025-03-25 09:53:05 -0500

    GFF-279: add TopBar story with config, required icons (#8)
    * add TopBar story with config, required icons
    
    * update to NextJS 15.2.3
    * add 0.10.84 TopBar fixes

---
+++ .storybook/loadIcons.ts
@@ -0,0 +1,34 @@
+import { type RegisteredIcons } from '@gen3/frontend';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const gen3Icons: RegisteredIcons = require(
+  `../config/icons/gen3.json`,
+);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colorIcons: RegisteredIcons = require(
+  `..//config/icons/color.json`,
+);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dictionaryIcons: RegisteredIcons = require(
+  `../config/icons/dataDictionary.json`,
+);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const workspaceIcons: RegisteredIcons = require(
+  `..//config/icons/workspace.json`,
+);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mmrfIcons: RegisteredIcons = require(
+  `..//config/icons/mmrf.json`,
+);
+
+
+const icons: RegisteredIcons[] = [
+  gen3Icons,
+  colorIcons,
+  dictionaryIcons,
+  workspaceIcons,
+  mmrfIcons
+];
+
+console.log(icons);
+
+export default icons;
