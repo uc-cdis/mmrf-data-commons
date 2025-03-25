952b35262edc05a4ff25cf2759c0ada3b47816e6
Craig Barnes <craig.barnes@intelligentlight.com>
2025-03-25 09:53:05 -0500

    GFF-279: add TopBar story with config, required icons (#8)
    * add TopBar story with config, required icons
    
    * update to NextJS 15.2.3
    * add 0.10.84 TopBar fixes

---
+++ src/components/Navigation/TopBar.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TopBar }  from '@gen3/frontend';
+
+const meta = {
+  component: TopBar,
+  title: 'Features/Navigation/TopBar',
+  parameters: {
+    deepControls: { enabled: true },
+  },
+} satisfies Meta<typeof TopBar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+
+    items: [
+      {
+        rightIcon: 'mmrf:video',
+        href: '/',
+        name: 'Video Guides',
+      },
+      {
+        rightIcon: 'mmrf:feedback',
+        href: '/',
+        name: 'Send Feedback',
+      },
+      {
+        rightIcon: 'mmrf:documentation',
+        href: '/',
+        name: 'Documentation',
+      },
+      {
+        rightIcon: 'mmrf:library',
+        href: '/',
+        name: 'My Data Library',
+      },
+    ],
+    loginButtonVisibility: 'visible' as any,
+    classNames: {
+      divider: 'border-base-min border-1',
+    },
+    itemClassnames :{
+        button: 'hover:border-accent'
+    }
+  },
+};
