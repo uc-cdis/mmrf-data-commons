217fa3fcc7c6331a77735b58f3d69724fcd37770
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-04-14 10:19:50 -0500

    feat(ssmsBasePage): Removed commented out code referencing removed files

--- src/features/GenomicTables/SomaticMutationsTable/TableComponents/index.ts
+++ src/features/GenomicTables/SomaticMutationsTable/TableComponents/index.ts
@@ -1,15 +1,4 @@
-// import SMTableDNAChange from "./SMTableDNAChange-UNUSED";
-// import SMTableSurvival from "./SMTableSurvival";
-// import SMTableCohort from "./SMTableCohort";
-// import SMTableProteinChange from "./SMTableProteinChange-UNUSED";
 import SMTableImpacts from './SMTableImpacts';
 import SMTableConsequences from './SMTableConsequences';
 
-export {
-  /* SMTableCohort,
-  SMTableDNAChange,
-  SMTableSurvival,
-  SMTableProteinChange, */
-  SMTableConsequences,
-  SMTableImpacts,
-};
+export { SMTableConsequences, SMTableImpacts };
