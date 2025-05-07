0c8d59316538af68ec4f1db1e90107461f497537
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-05-07 14:28:57 -0500

    feat(geneSummarybasePage): Began porting Gene Summary page

---
+++ src/pages/genes/[gene].tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import PageTitle from '@/components/PageTitle';
+import MainNavigation from '@/components/Navigation/MainNavigation/MainNavigation';
+import { GeneSummary } from '@/features/GeneSummary/GeneSummary';
+// import { headerElements } from "@/features/user-flow/workflow/navigation-utils";
+// import { UserFlowVariedPages } from "@/features/layout/UserFlowVariedPages";
+
+const GenesPage: NextPage = () => {
+  const router = useRouter();
+  const gene = router.asPath.split('/')[2]?.split('?')?.[0];
+
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setReady(true);
+    }
+  }, [router]);
+
+  return (
+    <>
+      <PageTitle pageName="Analysis Center" />
+      <div className="w-full flex-col flex gap-4 fixed z-10 bg-white">
+        <MainNavigation />
+      </div>
+      <h1 className="sr-only">Mutation Summary</h1>
+      <div className="flex">
+        <div className="w-full mt-[100px]">
+          {ready && <GeneSummary gene_id={gene} />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GenesPage;
