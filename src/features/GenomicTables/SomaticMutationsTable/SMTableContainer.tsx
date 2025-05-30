a944e52fb91b80d81251044450af64bd5a32044e
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-05-30 14:32:20 -0500

    feat(geneSummarySomaticMutationTable): fixed bug with size and total displayed following user search

--- src/features/GenomicTables/SomaticMutationsTable/SMTableContainer.tsx
+++ src/features/GenomicTables/SomaticMutationsTable/SMTableContainer.tsx
@@ -607,7 +607,7 @@ export const SMTableContainer: React.FC<SMTableContainerProps> = ({
               tableTotalDetail={
                 <TotalItems
                   // total={data?.ssmsTotal}
-                  total={displayedDataAfterSearch?.length + 1}
+                  total={displayedDataAfterSearch?.length}
                   itemName="somatic mutation"
                 />
               }
@@ -615,7 +615,7 @@ export const SMTableContainer: React.FC<SMTableContainerProps> = ({
               pagination={{
                 page,
                 pages,
-                size,
+                size: displayedDataAfterSearch?.length,
                 from,
                 total,
                 label: 'somatic mutation',
