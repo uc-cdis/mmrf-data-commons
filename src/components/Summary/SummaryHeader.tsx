c97ba3eb6eeea4bc16fb9afaaec9a50ca49b5d21
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2025-06-13 09:11:15 -0500

    Feat/external ref table icon (#30)
    * feat(externalRefTableIcon): Initial commit
    
    * feat(externalRefTableIcon): added uppercase class to make appearence consistent

--- src/components/Summary/SummaryHeader.tsx
+++ src/components/Summary/SummaryHeader.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from 'react';
 import Image from 'next/image';
 
 export interface SummaryHeaderProps {
+  iconPath: string;
   headerTitleLeft: string;
   headerTitle: string | number;
   leftElement?: ReactNode;
@@ -12,8 +13,9 @@ export interface SummaryHeaderProps {
   isModal?: boolean;
 }
 export const SummaryHeader = ({
-
-                                headerTitleLeft, headerTitle,
+  iconPath,
+  headerTitleLeft,
+  headerTitle,
   leftElement,
   rightElement,
   isModal = false,
@@ -32,14 +34,14 @@ export const SummaryHeader = ({
         >
           <Image
             className="ml-[8px] mt-[11px]"
-            src="/icons/gene-mutation.svg"
+            src={iconPath}
             alt=""
             height={40}
             width={40}
             data-testid="summary-header-icon"
           />
         </span>
-        <SummaryHeaderTitle data-testid="summary-header-title">
+        <SummaryHeaderTitle data-testid="summary-header-title" className='uppercase'>
           {headerTitleLeft} <span className="mx-4 text-2xl inline">•</span>{' '}
           {headerTitle}
         </SummaryHeaderTitle>
