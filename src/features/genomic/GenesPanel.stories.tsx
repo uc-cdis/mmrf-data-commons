f6ed90a7da8616c1ec790a6b9a972a51a166d537
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-07-25 12:59:32 -0500

    feat(mutationFreqAppGenePanel): Fixed storybook

--- src/features/genomic/GenesPanel.stories.tsx
+++ src/features/genomic/GenesPanel.stories.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { expect, within } from '@storybook/test';
 import { GenesPanel } from './GenesPanel';
-import { registerGenesAndMutationFrequencyAnalysisTool} from '@/features/genomic/registerApp';
+import { registerGenesAndMutationFrequencyAnalysisTool } from '@/features/genomic/registerApp';
 import Gen3GDCCompatabilityProvider from '@/utils/providers';
 import { Gen3Provider } from '@gen3/frontend';
+import { ComparativeSurvival } from './types';
 
 const meta = {
   component: GenesPanel,
@@ -19,40 +20,44 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => (
+  args: {
+    topGeneSSMSSuccess: false,
+    comparativeSurvival: {} as ComparativeSurvival,
+    handleSurvivalPlotToggled: () => {},
+    handleGeneAndSSmToggled: () => null,
+    handleMutationCountClick: () => null,
+  },
+  render: (args) => (
     <div style={{ padding: '20px' }}>
       <Gen3Provider
-              icons={[]}
-              sessionConfig={{
-  "updateSessionTime": 5,
-  "inactiveTimeLimit": 20,
-  "logoutInactiveUsers": false,
-  "monitorWorkspace": false
-}}
-              modalsConfig={{
-  "systemUseModal": {
-    "title": "",
-    "content": {
-      "text": [""]
-    },
-    "expireDays": 0,
-    "showOnlyOnLogin": true
-  }
-}}
-            >
-              <Gen3GDCCompatabilityProvider>
-                <GenesPanel  />
-              </Gen3GDCCompatabilityProvider>
-            </Gen3Provider>
-
+        icons={[]}
+        sessionConfig={{
+          updateSessionTime: 5,
+          inactiveTimeLimit: 20,
+          logoutInactiveUsers: false,
+          monitorWorkspace: false,
+        }}
+        modalsConfig={{
+          systemUseModal: {
+            title: '',
+            content: {
+              text: [''],
+            },
+            expireDays: 0,
+            showOnlyOnLogin: true,
+          },
+        }}
+      >
+        <Gen3GDCCompatabilityProvider>
+          <GenesPanel {...args} />
+        </Gen3GDCCompatabilityProvider>
+      </Gen3Provider>
     </div>
   ),
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const testIds = [
-      'genes-panel'
-    ];
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    const testIds = ['genes-panel'];
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     testIds.forEach((id) => {
       const currEle = canvas.getByTestId(id);
       expect(currEle).toBeInTheDocument();
