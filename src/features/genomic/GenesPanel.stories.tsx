23d64abd8e8688ac7be0cda945a8c1ca98d8d89d
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2025-08-05 12:02:37 -0500

    feat(mutationFreqAppGenePanel): Initial commit (#52)
    * feat(mutationFreqAppGenePanel): Initial commit
    
    * feat(mutationFreqAppGenePanel): everything running in browser now
    
    * feat(mutationFreqAppGenePanel): Fixes duplicate footer issue
    
    * feat(mutationFreqAppGenePanel): Have TSV download button partially functioning: downloads TSV with correct headers but missing data
    
    * feat(mutationFreqAppGenePanel): TSV DL works now, JSON DL works now
    
    * feat(mutationFreqAppGenePanel): Resolved issue with GenesTableCohort SVGs
    
    * feat(mutationFreqAppGenePanel): Fixed broken symbol icon
    
    * feat(mutationFreqAppGenePanel): Fixed double checkmark issue
    
    * feat(mutationFreqAppGenePanel): Added text versions for charts
    
    * feat(mutationFreqAppGenePanel): Got pagination to work with a single payload
    
    * feat(mutationFreqAppGenePanel): Got search to work
    
    * add all MMRF Gene table data (#53)
    
    * feat(mutationFreqAppGenePanel): Added mock hook to generate mock data for subrows
    
    * feat(mutationFreqAppGenePanel): Mocked handleSwitch function
    
    * feat(mutationFreqAppGenePanel): Overrode tailwind styles with CSS in app.css
    
    * feat(mutationFreqAppGenePanel): Overrode tailwind styles with CSS in app.css to align svg better
    
    * feat(mutationFreqAppGenePanel): Code cleanup to reduce changelist size, improve formatting, fix TS errors
    
    * feat(mutationFreqAppGenePanel): resolved TS issues in GenesTableContainer, still need to address  line 168, 169
    
    * update tests that do not compile (#56)
    
    * feat(mutationFreqAppGenePanel): resolved TS issue in GenesAndMutationFrequencyAnalysisTool regarding tabKey type
    
    * feat(mutationFreqAppGenePanel): resolved TS issue in mockedHooks related to import of non-existant file
    
    * feat(mutationFreqAppGenePanel): resolved TS issue in ToggledCheck.tsx
    
    * feat(mutationFreqAppGenePanel): Moved Annotations ICON to public folder, removed duplicate code in Table Components, resolved TS issues in utils.tsx
    
    * feat(mutationFreqAppGenePanel): Resolved additional type issue by commenting out filtering code and using different type defination for Survival
    
    * feat(mutationFreqAppGenePanel): Fixed accessibility issue, created storybook
    
    * feat(mutationFreqAppGenePanel): Fixed storybook
    
    * feat(mutationFreqAppGenePanel): removed console logs, unneeded line breaks
    
    * feat(mutationFreqAppGenePanel): autoformatted code
    
    ---------
    
    Co-authored-by: Craig Barnes <craig.barnes@intelligentlight.com>

---
+++ src/features/genomic/GenesPanel.stories.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
+import { GenesPanel } from './GenesPanel';
+import { registerGenesAndMutationFrequencyAnalysisTool } from '@/features/genomic/registerApp';
+import Gen3GDCCompatabilityProvider from '@/utils/providers';
+import { Gen3Provider } from '@gen3/frontend';
+import { ComparativeSurvival } from './types';
+
+const meta = {
+  component: GenesPanel,
+  title: 'features/GenesPanel',
+  parameters: {
+    deepControls: { enabled: true },
+  },
+} satisfies Meta<typeof GenesPanel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    topGeneSSMSSuccess: false,
+    comparativeSurvival: {} as ComparativeSurvival,
+    handleSurvivalPlotToggled: () => {},
+    handleGeneAndSSmToggled: () => null,
+    handleMutationCountClick: () => null,
+  },
+  render: (args) => (
+    <div style={{ padding: '20px' }}>
+      <Gen3Provider
+        icons={[]}
+        sessionConfig={{
+          updateSessionTime: 5,
+          inactiveTimeLimit: 20,
+          logoutInactiveUsers: false,
+          monitorWorkspace: false,
+        }}
+        modalsConfig={{
+          systemUseModal: {
+            title: '',
+            content: {
+              text: [''],
+            },
+            expireDays: 0,
+            showOnlyOnLogin: true,
+          },
+        }}
+      >
+        <Gen3GDCCompatabilityProvider>
+          <GenesPanel {...args} />
+        </Gen3GDCCompatabilityProvider>
+      </Gen3Provider>
+    </div>
+  ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testIds = ['genes-panel'];
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    testIds.forEach((id) => {
+      const currEle = canvas.getByTestId(id);
+      expect(currEle).toBeInTheDocument();
+    });
+  },
+};
---
+++ src/features/genomic/GenesPanel.tsx
@@ -0,0 +1,122 @@
+import { LoadingOverlay } from '@mantine/core';
+// import { GeneFrequencyChart } from "@/features/charts/GeneFrequencyChart";
+import { SurvivalPlotTypes } from '@/features/charts/SurvivalPlot/types';
+import React, { useCallback } from 'react';
+import { useDeepCompareMemo } from 'use-deep-compare';
+import {
+  ComparativeSurvival,
+  emptySurvivalPlot,
+} from '@/features/genomic/types';
+import {
+  useSelectFilterContent,
+  // useGeneAndSSMPanelData,
+} from '@/features/genomic/hooks';
+import { useGeneAndSSMPanelData } from './mockedHooks';
+import dynamic from 'next/dynamic';
+import { GeneFrequencyChart } from '../charts/GeneFrequencyChart';
+import { GenesTableContainer } from '../GenomicTables/GenesTable/GenesTableContainer';
+
+const SurvivalPlot = dynamic(
+  () => import('../charts/SurvivalPlot/SurvivalPlot'),
+  {
+    ssr: false,
+  },
+);
+
+interface GenesPanelProps {
+  topGeneSSMSSuccess: boolean;
+  comparativeSurvival: ComparativeSurvival;
+  handleSurvivalPlotToggled: (
+    symbol: string,
+    name: string,
+    field: string,
+  ) => void;
+  handleGeneAndSSmToggled: (
+    cohortStatus: string[],
+    field: string,
+    idField: string,
+    payload: Record<string, any>,
+  ) => void;
+  handleMutationCountClick: (geneId: string, geneSymbol: string) => void;
+}
+
+export const GenesPanel = ({
+  topGeneSSMSSuccess,
+  comparativeSurvival,
+  handleSurvivalPlotToggled,
+  handleGeneAndSSmToggled,
+  handleMutationCountClick,
+}: GenesPanelProps): JSX.Element => {
+  const {
+    isDemoMode,
+    currentCohortFilters,
+    genomicFilters,
+    overwritingDemoFilter,
+    survivalPlotData,
+    survivalPlotFetching,
+    survivalPlotReady,
+  } = useGeneAndSSMPanelData(comparativeSurvival, true);
+  const cohortFilters = currentCohortFilters;
+
+  const currentGenes = useSelectFilterContent('genes.gene_id');
+  const toggledGenes = useDeepCompareMemo(() => currentGenes, [currentGenes]);
+  const handleGeneToggled = useCallback(
+    (idAndSymbol: Record<string, any>) =>
+      handleGeneAndSSmToggled(
+        toggledGenes,
+        'genes.gene_id',
+        'geneID',
+        idAndSymbol,
+      ),
+    [handleGeneAndSSmToggled, toggledGenes],
+  );
+
+  return (
+    <div className="flex flex-col" data-testid="genes-panel">
+      <div className="flex flex-col gap-6 xl:gap-8 xl:flex-row bg-base-max mb-4">
+        <div className="w-full xl:w-1/2 border border-base-lighter p-4">
+          <GeneFrequencyChart
+            marginBottom={95}
+            genomicFilters={genomicFilters}
+            cohortFilters={isDemoMode ? overwritingDemoFilter : cohortFilters}
+          />
+        </div>
+        <div className="w-full xl:w-1/2 relative border border-base-lighter p-4">
+          <LoadingOverlay
+            zIndex={0}
+            data-testid="loading-spinner"
+            visible={
+              survivalPlotFetching ||
+              (!survivalPlotReady && !topGeneSSMSSuccess)
+            }
+          />
+          <SurvivalPlot
+            plotType={SurvivalPlotTypes.gene}
+            data={
+              survivalPlotReady && survivalPlotData.survivalData.length > 1
+                ? survivalPlotData
+                : emptySurvivalPlot
+            }
+            names={
+              survivalPlotReady && comparativeSurvival
+                ? [comparativeSurvival.symbol]
+                : []
+            }
+            field="gene.symbol"
+            tableTooltip
+          />
+        </div>
+      </div>
+      <GenesTableContainer
+        selectedSurvivalPlot={comparativeSurvival}
+        handleSurvivalPlotToggled={handleSurvivalPlotToggled}
+        handleGeneToggled={handleGeneToggled}
+        toggledGenes={toggledGenes}
+        genomicFilters={genomicFilters}
+        cohortFilters={isDemoMode ? overwritingDemoFilter : cohortFilters}
+        isDemoMode={isDemoMode}
+        handleMutationCountClick={handleMutationCountClick}
+      />
+    </div>
+  );
+};
