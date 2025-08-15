3a8b2bb00b3fa45d5be3dc88f02bb30d41f9601f
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-08-15 12:00:04 -0500

    feat(mutationFreqAppMutationsPanel): Fixed accessibility issue tabs color when not active

--- src/features/genomic/constants.ts
+++ src/features/genomic/constants.ts
@@ -4,5 +4,5 @@ export const SecondaryTabStyle = `flex flex-row items-center bg-base-max text-pr
   border-solid border-primary hover:bg-secondary-darker
   hover:text-white rounded-md first:border-r-0
   last:border-l-0 first:rounded-r-none last:rounded-l-none
-  hover:border-secondary data-active:bg-secondary
+  hover:border-secondary data-active:bg-secondary data-active:text-white
   data-active:text-secondary-content-max`;
