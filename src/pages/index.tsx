5ce8239a0e9ee618e1a002fead54c188e347d947
Paribartan Dhakal <paribartandhakal@Paribartans-CTDS-MacBook-Pro.local>
2025-11-12 17:46:49 -0500

    undo some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,7 +94,7 @@ const Tools = ({ sections, classNames }: AnalysisPageLayoutProps) => {
       <ProtectedContent>
         <div className="flex flex-col">
           <CohortManager
-            rightPanel={<CountsPanel index="case" indexPrefix="CaseCentric_" />}
+            rightPanel={<CountsPanel index="case_centric" indexPrefix="CaseCentric_" />}
             size="md"
             customActions={getCustomCohortButtons()}
           />
