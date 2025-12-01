a71ba9e632cfee4635c42883cf2a9fb0b412d0d0
siosonel <siosonel@gmail.com>
2025-12-01 07:21:29 -0800

    create cards for ProteinPaint-based analysis tools

---
+++ src/features/apps/OncoMatrix.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { MatrixWrapper } from "../proteinpaint/MatrixWrapper";
+
+const MatrixApp: FC = () => {
+  return <MatrixWrapper chartType="matrix" />;
+};
+
+export default MatrixApp;
---
+++ src/features/proteinpaint/sjpp-types.tsx
@@ -0,0 +1,56 @@
+import { FilterSet } from "@gff/core";
+
+export interface PpApi {
+  update(arg: any): null;
+  staleInstance?: boolean;
+  getState?: () => any;
+}
+
+export type SampleData = {
+  "cases.case_id"?: string;
+};
+
+export interface SelectSamplesCallBackArg {
+  samples: SampleData[];
+  source: string;
+}
+
+export type SelectSamplesCallback = (samples: SelectSamplesCallBackArg) => void;
+
+export type attrMapping = {
+  from: string;
+  to?: string;
+  convert?: boolean;
+};
+
+export interface SelectSamples {
+  buttonText: string;
+  attributes: attrMapping[];
+  callback?: SelectSamplesCallback;
+}
+
+export function getFilters(arg: SelectSamplesCallBackArg): FilterSet {
+  const { samples } = arg;
+  // see comments below about SV-2228
+  const ids = samples.map((d) => d["cases.case_id"]).filter((d) => d && true);
+  return {
+    mode: "and",
+    root: {
+      // see https://jira.opensciencedatacloud.org/browse/SV-2228
+      // per Craig: I suggest always representing cohorts comprised of cases using the cases.case_id filter,
+      // as it is the most general of the case filters. Within an app, you can use any filter needed;
+      // when these become part of the cohort, there is some potential for issues.
+      // NOTE: This is primarily because both the frontend and backends are trying to, in effect,
+      // JOINs across the explore and repository indexes, and there is some additional work needed to work will all filters.
+      "cases.case_id": {
+        operator: "includes",
+        field: "cases.case_id",
+        operands: ids,
+      },
+    },
+  };
+}
+
+export type RxComponentCallbacks = {
+  [eventName: string]: () => void;
+};
