767763c3c859a99cc2afc6c4a93d459b87b2dbe6
craigrbarnes <craig.barnes@intelligentlight.com>
2026-02-13 09:46:48 -0600

    refactor modals to use Mantine context-based modal system

--- src/components/Modals/NoAccessModal.tsx
+++ src/components/Modals/NoAccessModal.tsx
@@ -1,35 +1,17 @@
 import React from "react";
 import { Text } from "@mantine/core";
-import { v4 as uuidv4 } from "uuid";
-import { LoginButton } from "../LoginButton";
-import { BaseModal } from "./BaseModal";
+import { ContextModalProps } from '@mantine/modals';
 
 export const NoAccessModal = ({
-                                openModal,
-                              }: {
-  openModal: boolean;
-}): JSX.Element => {
+  context,
+  id,
+  innerProps,
+}: ContextModalProps) => {
   return (
-    <BaseModal
-      openModal={openModal}
-      title={
-        <Text size="lg" className="font-medium">
-          Access Alert
-        </Text>
-      }
-      buttons={[
-        {
-          title: "Close",
-          dataTestId: "button-no-access-modal-access-alert-close",
-        },
-        <LoginButton fromSession key={uuidv4()} />,
-      ]}
-    >
-      <div className="border-y border-y-base p-4">
-        <Text size="sm">
-          You don&apos;t have access to this file. Please login.
-        </Text>
-      </div>
-    </BaseModal>
+    <div className="border-y border-y-base p-4">
+      <Text size="sm">
+        You don&apos;t have access to this file. Please login.
+      </Text>
+    </div>
   );
 };
