e2dbf9a59e12cfd3b1b36e79ac3e670e42cab371
siosonel <siosonel@gmail.com>
2026-03-18 13:30:34 -0500

    revert the deleted @swc optional entries in package-lock

--- next.config.js
+++ next.config.js
@@ -29,10 +29,10 @@ const nextConfig = {
   trailingSlash: true,
   experimental: {
     esmExternals: true,
-    // support npm-linked modules like protenpaint-client that are tracked under the parent directory
-    // of the mmrf-data-commons repo, not within it
-    outputFileTracingRoot: process.env.PROTEINPAINT_API ? path.join(__dirname, '../') : undefined
   },
+  // support npm-linked modules like protenpaint-client that are tracked under the parent directory
+  // of the mmrf-data-commons repo, not within it
+  outputFileTracingRoot: process.env.PROTEINPAINT_API ? path.join(__dirname, '../') : undefined,
   transpilePackages: ['@gen3/core', '@gen3/frontend'],
   webpack: (config) => {
     config.infrastructureLogging = {
--- src/features/proteinpaint/dev.sh
+++ src/features/proteinpaint/dev.sh
@@ -6,8 +6,8 @@
 if [[ "$1" == "unlink" ]]; then
 	# to test the published client package before submitting a PR with an updated pp-client version
 	npm unlink ../proteinpaint/client
-	npm uninstall @sjcrh/proteinpaint-client --save --workspace=packages/portal-proto
-	npm install @sjcrh/proteinpaint-client --save --save-exact --workspace=packages/portal-proto
+	npm uninstall @sjcrh/proteinpaint-client --save
+	npm install @sjcrh/proteinpaint-client --save
 
 else
 	# to test the local PP client code
@@ -15,7 +15,7 @@ else
 	# An issue with npm link and workspaces: the non-linked @sjcrh/proteinpaint-client package
 	# may be moved to portal-proto/node_modules, creating 2 separate modules of the same package,
 	# must ensure only the linked module is used for bundling so delete
-	# rm -rf packages/portal-proto/node_modules/@sjcrh
+	rm -rf node_modules/@sjcrh
 	# also not able to do a simpler
 	# `cd packages/portal-proto && npm link path/to/proteinpaint/client`,
 	# where the linked module would be in portal-proto/node_modules instead of the
