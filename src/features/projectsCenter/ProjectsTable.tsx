ec73ffaff40be2111aa3477ef2901cab145f9614
rodigerj-mmrf <rodigerj@themmrf.org>
2026-03-24 14:30:03 -0400

    Remove Experimental Strategy, Disease Type, Primary Site columns from Projects Page table

--- src/features/projectsCenter/ProjectsTable.tsx
+++ src/features/projectsCenter/ProjectsTable.tsx
@@ -1,37 +1,29 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { useDeepCompareMemo } from 'use-deep-compare';
-import { EmptyFilterSet, FilterSet } from '@gen3/core';
+import { EmptyFilterSet } from '@gen3/core';
 import { ProjectDefaults, SortBy } from '@/core';
 
-import { PopupIconButton } from '@/components/PopupIconButton/PopupIconButton';
 import OverflowTooltippedLabel from '@/components/OverflowTooltippedLabel';
-import { extractToArray, statusBooleansToDataStatus } from 'src/utils';
-import { ArraySeparatedSpan } from '@/components/ArraySeparatedSpan/ArraySeparatedSpan';
+import { statusBooleansToDataStatus } from 'src/utils';
 import VerticalTable from '@/components/Table/VerticalTable';
 import {
   ColumnDef,
   ColumnOrderState,
   createColumnHelper,
-  ExpandedState,
   SortingState,
   VisibilityState,
 } from '@tanstack/react-table';
 
 import { HandleChangeInput } from '@/components/Table/types';
-import SubrowPrimarySiteDiseaseType from '@/components/SubrowPrimarySiteDiseaseType/SubrowPrimarySiteDiseaseType';
 import TotalItems from '@/components/Table/TotalItem';
 import { useProjectsQuery } from '@/core/features/projects/projectsSlice';
 import { Image } from '@/components/Image';
 import Link from 'next/link';
-import { isArray } from 'lodash';
 
 type ProjectDataType = {
   project: string;
-  disease_type: string[];
-  primary_site: string[];
   program: string;
   cases: number;
-  experimental_strategy: (string | number)[];
   files: string;
 };
 
@@ -80,36 +72,10 @@ const ProjectsTable: React.FC = () => {
 
       return [
         projectsData?.projects?.map(
-          ({
-            project_id,
-            disease_type,
-            primary_site,
-            program,
-            summary,
-          }: ProjectDefaults) => ({
+          ({ project_id, program, summary }: ProjectDefaults) => ({
             project: project_id,
-            disease_type: [...disease_type].sort((a, b) =>
-              a.toLowerCase().localeCompare(b.toLowerCase()),
-            ),
-            primary_site: [...primary_site].sort((a, b) =>
-              a.toLowerCase().localeCompare(b.toLowerCase()),
-            ),
             program: program?.name,
             cases: summary?.case_count ?? 0,
-            experimental_strategy:
-              (summary?.experimental_strategies &&
-              isArray(summary?.experimental_strategies))
-                ? (
-                    [
-                      ...extractToArray(
-                        summary?.experimental_strategies,
-                        'experimental_strategy',
-                      ),
-                    ] as string[]
-                  ).sort((a, b) =>
-                    a.toLowerCase().localeCompare(b.toLowerCase()),
-                  )
-                : [],
             files: summary?.file_count.toLocaleString(),
           }),
         ) as ProjectDataType[],
@@ -137,9 +103,6 @@ const ProjectsTable: React.FC = () => {
         },
       ];
   }, [isSuccess, isFetching, projectsData]);
-
-  const [expanded, setExpanded] = useState<ExpandedState>({});
-  const [expandedColumnId, setExpandedColumnId] = useState(null);
 
   const projectsTableDefaultColumns = useMemo<
     ColumnDef<ProjectDataType, any>[]
@@ -177,16 +140,6 @@ const ProjectsTable: React.FC = () => {
         },
         enableSorting: true,
       }),
-      projectsTableColumnHelper.accessor('disease_type', {
-        id: 'disease_type',
-        header: 'Disease Type',
-        enableSorting: false,
-      }),
-      projectsTableColumnHelper.accessor('primary_site', {
-        id: 'primary_site',
-        header: 'Primary Site',
-        enableSorting: false,
-      }),
       projectsTableColumnHelper.accessor('program', {
         id: 'program',
         header: 'Program',
@@ -203,21 +156,13 @@ const ProjectsTable: React.FC = () => {
         cell: ({ getValue }) => getValue().toLocaleString().padStart(9),
         enableSorting: true,
       }),
-      projectsTableColumnHelper.accessor('experimental_strategy', {
-        id: 'experimental_strategy',
-        header: 'Experimental Strategy',
-        cell: ({ getValue }) => (
-          <ArraySeparatedSpan data={getValue() as string[]} />
-        ),
-        enableSorting: false,
-      }),
       projectsTableColumnHelper.accessor('files', {
         id: 'files',
         header: 'Files',
         enableSorting: true,
       }),
     ],
-    [expandedColumnId, activePage, projectsData?.totalProjects],
+    [],
   );
 
   const getRowId = (originalRow: ProjectDataType) => {
@@ -225,9 +170,6 @@ const ProjectsTable: React.FC = () => {
   };
 
   const [rowSelection, setRowSelection] = useState({});
-  const pickedProjects = Object.entries(rowSelection)?.map(
-    ([project]) => project,
-  );
   const [columnOrder, setColumnOrder] = useState<ColumnOrderState>(
     projectsTableDefaultColumns.map((column) => column.id as string), //must start out with populated columnOrder so we can splice
   );
@@ -252,12 +194,10 @@ const ProjectsTable: React.FC = () => {
         break;
       case 'newPageNumber':
         setActivePage(obj.newPageNumber ?? 0);
-        setExpanded({});
         break;
       case 'newSearch':
         setSearchTerm(obj.newSearch ?? '');
         setActivePage(1);
-        setExpanded({});
         break;
     }
   };
@@ -300,20 +240,6 @@ const ProjectsTable: React.FC = () => {
   //   });
   // };
 
-  // const handleExpand = (row: Row<ProjectDataType>, columnId: string) => {
-  //   if (
-  //     Object.keys(expanded).length > 0 &&
-  //     row.original.project === expandedRowId &&
-  //     columnId === expandedColumnId
-  //   ) {
-  //     setExpanded({});
-  //   } else if ((row.original[columnId] as string[]).length > 1) {
-  //     setExpanded({ [row.original.project]: true });
-  //     setExpandedColumnId(columnId);
-  //     setExpandedRowId(row.original.project);
-  //   }
-  // };
-
   return (
     <VerticalTable
       tableTotalDetail={
@@ -330,11 +256,6 @@ const ProjectsTable: React.FC = () => {
         enabled: false,
         tooltip: 'e.g. MMRF-IA19',
       }}
-      getRowCanExpand={() => true}
-      expandableColumnIds={['disease_type', 'primary_site']}
-      renderSubComponent={({ row, clickedColumnId }) => (
-        <SubrowPrimarySiteDiseaseType row={row} columnId={clickedColumnId} />
-      )}
       status={statusBooleansToDataStatus(isFetching, isSuccess, isError)}
       handleChange={handleChange}
       enableRowSelection={true}
