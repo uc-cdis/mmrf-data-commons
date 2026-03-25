5f72e91595560dcc833940837a8bf80e8f1e72a8
craigrbarnes <craig.barnes@intelligentlight.com>
2026-03-25 09:27:56 -0500

    Add `download` attribute to enable direct file download

--- src/components/DownloadButtons/DownloadPresignedURL.tsx
+++ src/components/DownloadButtons/DownloadPresignedURL.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from '@mantine/core';
-import saveAs from 'file-saver';
 import { GEN3_FENCE_API } from '@gen3/core';
 import { MMRFFile } from '@/core/features/files/filesSlice';
 
@@ -15,30 +14,15 @@ const DownloadPresignedURL = ({
   customDataTestID,
   label,
 }: DownloadPresignedURLProps) => {
-  const handleDownload = async () => {
-    const url = `${GEN3_FENCE_API}/data/download/${
-      file.file_id ? encodeURIComponent(file.file_id as string) : ''
-    }?redirect=true&expires_in=9600`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      credentials: 'include',
-    });
-
-    if (!response.ok) {
-      throw new Error(`Download failed: ${response.status}`);
-    }
-
-    const blob = await response.blob();
-    saveAs(blob, file.file_name, { autoBom: false });
-  };
-
   return (
     <Button
       test-id={customDataTestID}
-      component="button"
-      onClick={handleDownload}
+      component="a"
+      download={file.file_name}
       color="primary.4"
+      href={`${GEN3_FENCE_API}/data/download/${
+        file.file_id ? encodeURIComponent(file.file_id as string) : ''
+      }?redirect=true&expires_in=9600`}
     >
       {label}
     </Button>
