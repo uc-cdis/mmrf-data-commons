d1b2e7f800f6d937c6e5f6d2bfdc58345c3e6228
rodigerj-mmrf <rodigerj@themmrf.org>
2026-04-01 16:09:32 -0400

    hide set operations in prod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,6 +32,7 @@ const PROD_HOSTNAME = 'virtuallab.themmrf.org';
 const PROD_HIDDEN_APP_IDS = new Set([
   'OncoMatrix',
   'GeneExpression',
+  'SetOperations',
 ]);
 
 interface CountsPanelProps {
